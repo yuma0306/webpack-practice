4e185fd78f9543daf147337e142cc85a36ff7855
yuma0306 <yuma0306895@gmail.com>
2022-02-21 23:47:04 +0900

    cssとHTMLのプラグイン追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -12,7 +14,8 @@ module.exports = {
                 test: /\.css/,
                 use: [
                     {
-                        loader: 'style-loader',
+                        // loader: 'style-loader',
+                        loader: MiniCssExtractPlugin.loader,
                     },
                     {
                         loader: 'css-loader',
@@ -21,4 +24,10 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
 }
