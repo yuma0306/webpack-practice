73c7653ad1670c76051d08e995ddd49e99e422d1
yuma0306 <yuma0306895@gmail.com>
2022-02-23 19:48:17 +0900

    file-loaderを追加

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.(png|jpg)/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'images/[name].[ext]',
+                        }
+                    },
+                ],
+            },
         ],
     },
     plugins: [
